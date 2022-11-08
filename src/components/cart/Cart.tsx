6699ac5326ebc0209a2578c49dd59c23b0c0c6f7
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-11-08 14:12:49 +0100

    change inner padding in Cart component

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -16,49 +16,47 @@ export const Cart = ({ cartIsOpen }: CartProps) => {
       ></div>
       <div
         className={cn(
-          "fixed right-0 bottom-0 z-10 max-w-xs md:max-w-md lg:max-w-lg w-full p-8 lg:p-0 flex flex-col gap-6 overflow-y-auto bg-white shadow-md transition duration-300 ease-in-out top-16 lg:top-20 border-t-2 border-zinc-200",
+          "fixed right-0 bottom-0 z-10 max-w-xs md:max-w-md lg:max-w-lg w-full p-6 md:p-8 flex flex-col gap-6 overflow-y-auto bg-white shadow-md transition duration-300 ease-in-out top-16 lg:top-20 border-t-2 border-zinc-200",
           { "translate-x-0": cartIsOpen },
           { "translate-x-full": !cartIsOpen },
         )}
       >
-        <div className="flex flex-col p-8">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
-            recusandae itaque cumque sunt aspernatur odio incidunt libero
-            similique laudantium culpa ut alias consequatur, assumenda placeat
-            tenetur obcaecati perferendis officia explicabo?
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
-            recusandae itaque cumque sunt aspernatur odio incidunt libero
-            similique laudantium culpa ut alias consequatur, assumenda placeat
-            tenetur obcaecati perferendis officia explicabo?
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
-            recusandae itaque cumque sunt aspernatur odio incidunt libero
-            similique laudantium culpa ut alias consequatur, assumenda placeat
-            tenetur obcaecati perferendis officia explicabo?
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
-            recusandae itaque cumque sunt aspernatur odio incidunt libero
-            similique laudantium culpa ut alias consequatur, assumenda placeat
-            tenetur obcaecati perferendis officia explicabo?
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
-            recusandae itaque cumque sunt aspernatur odio incidunt libero
-            similique laudantium culpa ut alias consequatur, assumenda placeat
-            tenetur obcaecati perferendis officia explicabo?
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
-            recusandae itaque cumque sunt aspernatur odio incidunt libero
-            similique laudantium culpa ut alias consequatur, assumenda placeat
-            tenetur obcaecati perferendis officia explicabo?
-          </p>
-        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+          recusandae itaque cumque sunt aspernatur odio incidunt libero
+          similique laudantium culpa ut alias consequatur, assumenda placeat
+          tenetur obcaecati perferendis officia explicabo?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+          recusandae itaque cumque sunt aspernatur odio incidunt libero
+          similique laudantium culpa ut alias consequatur, assumenda placeat
+          tenetur obcaecati perferendis officia explicabo?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+          recusandae itaque cumque sunt aspernatur odio incidunt libero
+          similique laudantium culpa ut alias consequatur, assumenda placeat
+          tenetur obcaecati perferendis officia explicabo?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+          recusandae itaque cumque sunt aspernatur odio incidunt libero
+          similique laudantium culpa ut alias consequatur, assumenda placeat
+          tenetur obcaecati perferendis officia explicabo?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+          recusandae itaque cumque sunt aspernatur odio incidunt libero
+          similique laudantium culpa ut alias consequatur, assumenda placeat
+          tenetur obcaecati perferendis officia explicabo?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+          recusandae itaque cumque sunt aspernatur odio incidunt libero
+          similique laudantium culpa ut alias consequatur, assumenda placeat
+          tenetur obcaecati perferendis officia explicabo?
+        </p>
       </div>
     </>
   );
