65f1a7fe7d2d792a237a8d19c4d4bdf7bbb185e3
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-11-08 12:28:29 +0100

    add padding to ProductsList component instead margin

--- src/components/productsList/ProductsList.tsx
+++ src/components/productsList/ProductsList.tsx
@@ -6,7 +6,7 @@ export const ProductsList = () => {
   const { products } = useProducts();
 
   return (
-    <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 my-4">
+    <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 py-12">
       {products.map((product) => (
         <Product key={product.id} {...product} />
       ))}
