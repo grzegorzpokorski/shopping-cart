785996ab0aeb699181a18bbfcb40bf48aad762c3
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-11-08 12:30:09 +0100

    implement Layout component in Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { Layout } from "../components/layout/Layout";
 import { ProductsList } from "../components/productsList/ProductsList";
 import { ProductsProvider } from "../context/ProductsContext";
 
 export const Home = () => {
   return (
     <ProductsProvider>
-      <div className="container mx-auto px-4">
+      <Layout>
         <ProductsList />
-      </div>
+      </Layout>
     </ProductsProvider>
   );
 };
