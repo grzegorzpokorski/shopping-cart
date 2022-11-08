4e06862f70fb3669274923827a23faf2d078caa3
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-11-08 14:38:38 +0100

    add a Footer component to Layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,7 @@
 import React, { ReactNode } from "react";
+import { Footer } from "../footer/Footer";
 import { Navbar } from "../navbar/Navbar";
+import { Link } from "../link/Link";
 
 type LayoutProps = {
   children: ReactNode;
@@ -12,6 +14,15 @@ export const Layout = ({ children }: LayoutProps) => {
         <Navbar />
       </header>
       <main className="container mx-auto px-4 mt-16 lg:mt-20">{children}</main>
+      <Footer
+        content={
+          <p className="text-sm text-center">
+            Copyright &copy; {new Date().getFullYear()}. Created by Grzegorz
+            Pokorski. Images belongs to{" "}
+            <Link href="https://www.logitech.com/">Logitech</Link>.
+          </p>
+        }
+      />
     </>
   );
 };
