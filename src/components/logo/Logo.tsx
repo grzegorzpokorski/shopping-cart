c3af964c28cffdcf423ba36009828233009faf6f
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-11-08 12:25:07 +0100

    add Logo component

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "../link/Link";
+
+type LogoProps = {
+  isHome?: boolean;
+  isTitle?: boolean;
+  content: string;
+};
+
+export const Logo = ({ isHome, isTitle, content }: LogoProps) => {
+  return (
+    <Link
+      href={`${isHome ? "#" : "/"}`}
+      className="transition hover:opacity-70"
+    >
+      {isTitle ? (
+        <h1 className="text-lg font-bold">{content}</h1>
+      ) : (
+        <span className="text-lg font-bold">{content}</span>
+      )}
+    </Link>
+  );
+};
