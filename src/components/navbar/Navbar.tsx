78e8090976351fcc7f549ad0c493bd62d6eb00ca
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-11-08 12:25:44 +0100

    add Navbar component

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { CartTrigger } from "../cartTrigger/CartTrigger";
+import { Logo } from "../logo/Logo";
+
+export const Navbar = () => {
+  const [isHome, setIsHome] = useState(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    if (pathname === "/") {
+      setIsHome(true);
+    }
+  }, [pathname]);
+
+  const [cartIsOpen, setCartIsOpen] = useState(false);
+  const toggleCart = () => setCartIsOpen((v) => !v);
+
+  return (
+    <nav className="fixed top-0 z-50 w-full bg-white shadow-md">
+      <section
+        className={`container mx-auto px-4 flex flex-row justify-between items-center h-16 lg:h-20`}
+      >
+        <Logo content="Shop" isHome={isHome} isTitle={isHome} />
+        <div>
+          <CartTrigger cartIsOpen={cartIsOpen} toggleCart={toggleCart} />
+          {/* <MainMenu mobileMenuIsOpen={mobileMenuIsOpen}/> */}
+        </div>
+      </section>
+    </nav>
+  );
+};
