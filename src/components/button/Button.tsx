d481ac857fc9cc45ac6659278a8dedace5a5a6be
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-11-09 00:31:38 +0100

    add variants to Button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,15 +1,34 @@
 import React from "react";
+import cn from "classnames";
 
 type ButtonProps = {
   text: string;
   disabled?: boolean;
+  variant?: ButtonVariants;
   onClick?: () => void;
 };
 
-export const Button = ({ text, disabled, onClick }: ButtonProps) => {
+type ButtonVariants = "default" | "indigo";
+
+const variants = {
+  default:
+    "bg-zinc-700 text-white text-md px-3 py-2 hover:bg-zinc-800 focus:bg-zinc-800 disabled:bg-zinc-400",
+  indigo:
+    "bg-indigo-500 text-white text-md px-3 py-2 hover:bg-indigo-600 focus:bg-indigo-600 disabled:bg-indigo-300",
+} as const;
+
+export const Button = ({
+  text,
+  disabled,
+  variant = "default",
+  onClick,
+}: ButtonProps) => {
   return (
     <button
-      className="bg-zinc-700 text-white text-md px-3 py-2 hover:bg-zinc-800 focus:bg-zinc-800 disabled:bg-zinc-400 disabled:cursor-not-allowed transition-colors"
+      className={cn(
+        variants[variant],
+        "disabled:cursor-not-allowed transition-colors",
+      )}
       disabled={disabled}
       onClick={onClick}
     >
