8696385a331a4880119a936ee6b9454c16becd39
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-11-09 23:14:49 +0100

    add additional buttons variants in Button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,24 +1,26 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import cn from "classnames";
 
 type ButtonProps = {
-  text: string;
+  children: ReactNode;
   disabled?: boolean;
   variant?: ButtonVariants;
   onClick?: () => void;
 };
 
-type ButtonVariants = "default" | "indigo";
+type ButtonVariants = "default" | "indigo" | "indigo_link" | "cart";
 
 const variants = {
   default:
     "bg-zinc-700 text-white text-md px-3 py-2 hover:bg-zinc-800 focus:bg-zinc-800 disabled:bg-zinc-400",
   indigo:
     "bg-indigo-500 text-white text-md px-3 py-2 hover:bg-indigo-600 focus:bg-indigo-600 disabled:bg-indigo-300",
+  indigo_link: "text-indigo-600 hover:text-indigo-500",
+  cart: "flex items-center border-2 border-zinc-400 hover:border-zinc-600 rounded text-zinc-400 hover:text-zinc-600 p-1 text-sm disabled:cursor-not-allowed disabled:hover:text-zinc-400 disabled:border-zinc-400",
 } as const;
 
 export const Button = ({
-  text,
+  children,
   disabled,
   variant = "default",
   onClick,
@@ -27,12 +29,12 @@ export const Button = ({
     <button
       className={cn(
         variants[variant],
-        "disabled:cursor-not-allowed transition-colors",
+        "disabled:cursor-not-allowed transition-colors rounded",
       )}
       disabled={disabled}
       onClick={onClick}
     >
-      {text}
+      {children}
     </button>
   );
 };
