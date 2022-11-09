6ec46c747656be09b3a993f02c7fcef0ae50bdd6
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-11-09 23:17:32 +0100

    add EmptyCart component

---
+++ src/components/emptyCart/EmptyCart.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Button } from "../button/Button";
+import { useShoppingCartContext } from "../../context/ShoppingCartContext";
+
+export const EmptyCart = () => {
+  const { shoppingCartDispatch } = useShoppingCartContext();
+
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <p>Dodaj coś do mnie, aby mnie uszczęśliwić ;)</p>
+      <Button onClick={() => shoppingCartDispatch({ type: "toggle_cart" })}>
+        Kontynuj zakupy
+      </Button>
+    </div>
+  );
+};
