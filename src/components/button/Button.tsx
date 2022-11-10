65336f509849fe0fa5e6dadb06e8a6310e9d3973
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-11-10 17:23:28 +0100

    change cart Button variant when is dissabled

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -16,7 +16,7 @@ const variants = {
   indigo:
     "bg-indigo-500 text-white text-md px-3 py-2 hover:bg-indigo-600 focus:bg-indigo-600 disabled:bg-indigo-300",
   indigo_link: "text-indigo-600 hover:text-indigo-500",
-  cart: "flex items-center border-2 border-zinc-400 hover:border-zinc-600 rounded text-zinc-400 hover:text-zinc-600 p-1 text-sm disabled:cursor-not-allowed disabled:hover:text-zinc-400 disabled:border-zinc-400",
+  cart: "flex items-center border-2 border-zinc-400 hover:border-zinc-600 rounded text-zinc-400 hover:text-zinc-600 p-1 text-sm disabled:cursor-not-allowed disabled:text-zinc-300 disabled:border-zinc-300 disabled:hover:text-zinc-300 disabled:hover:border-zinc-300",
 } as const;
 
 export const Button = ({
