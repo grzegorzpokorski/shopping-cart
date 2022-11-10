e7801677ad2c284ddefdf15a1ee3a32d1042eb22
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-11-10 23:31:04 +0100

    isolate CartListItem from CartList

---
+++ src/components/cartListItem/CartListItem.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+import {
+  ProductInCart,
+  useShoppingCartContext,
+} from "../../context/ShoppingCartContext";
+import { formatCurrency } from "../../utils/formatCurrency";
+import { Button } from "../button/Button";
+import { FaMinus, FaPlus, FaTrashAlt } from "react-icons/fa";
+
+export const CartListItem = ({
+  id,
+  name,
+  price,
+  type,
+  image,
+  availableAmount,
+  qty,
+}: ProductInCart) => {
+  const { shoppingCartDispatch } = useShoppingCartContext();
+
+  return (
+    <li className="flex flex-col gap-4 py-6" key={id}>
+      <div className="flex items-center justify-between gap-4">
+        <div className="flex gap-4 items-center">
+          <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded border border-gray-200">
+            <img
+              src={image.url}
+              alt={image.alt}
+              height={image.height}
+              width={image.width}
+            />
+          </div>
+          <div className="flex flex-col text-sm text-gray-900">
+            <h3 className="font-bold">{name}</h3>
+            <p className="mt-1 text-sm text-gray-500">{type}</p>
+          </div>
+        </div>
+        <Button
+          variant="indigo_link"
+          onClick={() =>
+            shoppingCartDispatch({
+              type: "remove_product_from_cart",
+              id: id,
+            })
+          }
+        >
+          <span className="sr-only">usuń z koszyka</span>
+          <FaTrashAlt aria-hidden="true" />
+        </Button>
+      </div>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-1">
+          <Button
+            variant="cart"
+            disabled={qty === 1}
+            onClick={() =>
+              shoppingCartDispatch({
+                type: "decrease_product_qty_in_cart",
+                id: id,
+              })
+            }
+          >
+            <span className="sr-only">zmniejsz</span>
+            <FaMinus className="text-center" />
+          </Button>
+          <span className="px-2 text-sm">
+            {qty}{" "}
+            <span className="hidden md:inline">
+              {" "}
+              z {availableAmount} dostępnych
+            </span>
+          </span>
+          <Button
+            variant="cart"
+            disabled={qty === availableAmount ? true : false}
+            onClick={() =>
+              shoppingCartDispatch({
+                type: "increase_product_qty_in_cart",
+                id: id,
+              })
+            }
+          >
+            <span className="sr-only">zwiększ</span>
+            <FaPlus className="text-center block" />
+          </Button>
+        </div>
+        <p className="text-sm">
+          <span className="font-bold">{formatCurrency(price * qty)}</span>
+          <br />
+          {qty > 1 ? `za sztukę ${formatCurrency(price)}` : null}
+        </p>
+      </div>
+    </li>
+  );
+};
