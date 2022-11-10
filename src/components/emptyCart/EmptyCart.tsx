b4aa168a5547accb2f6bc5eab6b181db770627b3
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-11-11 00:47:45 +0100

    change EmptyCart content

--- src/components/emptyCart/EmptyCart.tsx
+++ src/components/emptyCart/EmptyCart.tsx
@@ -7,7 +7,7 @@ export const EmptyCart = () => {
 
   return (
     <div className="flex flex-col items-center text-center gap-4">
-      <p>Dodaj coś do mnie, aby mnie uszczęśliwić ;)</p>
+      <p>Dodaj do koszyka przedmioty i kup je szybko i wygodnie.</p>
       <Button onClick={() => shoppingCartDispatch({ type: "toggle_cart" })}>
         Kontynuj zakupy
       </Button>
