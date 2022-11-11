42117167b937e64818855c0beebc1f1f2c012e0e
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-11-11 20:31:26 +0100

    remove temp SortProducts component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import { Layout } from "../components/templates/layout/Layout";
 import { ProductsList } from "../components/organisms/productsList/ProductsList";
 import { ShoppingCartProvider } from "../context/ShoppingCartContext";
-import { SortProducts } from "../components/molecules/sortProducts/SortProducts";
 
 export const Home = () => {
   return (
     <ShoppingCartProvider>
       <Layout>
         <>
-          <SortProducts />
           <ProductsList />
         </>
       </Layout>
