418dff036002251cb865f5c65b1fbbc31840a70a
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-11-12 10:51:31 +0100

    change color of current menu location

--- src/components/molecules/mainMenu/MainMenu.tsx
+++ src/components/molecules/mainMenu/MainMenu.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import cn from "classnames";
+import { useLocation } from "react-router-dom";
 import { Link } from "../../atoms/link/Link";
 
 type MainMenuProps = {
@@ -10,11 +12,19 @@ type MainMenuProps = {
 };
 
 export const MainMenu = ({ items }: MainMenuProps) => {
+  const { pathname } = useLocation();
+  console.log(pathname);
+
   return (
     <ul className="flex items-center gap-5">
       {items.map((item) => (
         <li key={item.label}>
-          <Link href={item.href} className="flex flex-row items-center gap-2">
+          <Link
+            href={item.href}
+            className={cn("flex flex-row items-center gap-2", {
+              "text-indigo-600": pathname === item.href,
+            })}
+          >
             <span className="hidden md:inline">{item.label}</span>
             <span className="sr-only md:hidden">{item.label}</span>
             <span className="flex items-center justify-center text-xl inline md:hidden">
