24f5461e20183ad085f332bf4810fdfd619424fc
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-11-21 19:53:14 +0100

    remove unused imports

--- src/components/molecules/productListItem/ProductListItem.test.tsx
+++ src/components/molecules/productListItem/ProductListItem.test.tsx
@@ -1,4 +1,4 @@
-import { cleanup, fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import {
   ProductType,
