b0f16c1dc8d8e7614d20a5582891ca3a24450afc
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-02-06 02:38:35 +0100

    feat: implement UIProvider

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./styles/style.scss";
 import { ShoppingCartProvider } from "./context/ShoppingCartContext";
+import { UIProvider } from "./providers/UIProvider";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ShoppingCartProvider>
-        <App />
-      </ShoppingCartProvider>
+      <UIProvider>
+        <ShoppingCartProvider>
+          <App />
+        </ShoppingCartProvider>
+      </UIProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
