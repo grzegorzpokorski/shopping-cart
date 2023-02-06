639232028a8abad94b414f865317e39889630b66
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-02-06 14:27:09 +0100

    update dependencies and fix eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,19 @@
+/**
+ * @type {import('eslint').Linter.Config}
+ */
+
 module.exports = {
+  root: true,
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["react", "@typescript-eslint"],
   extends: [
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
     "plugin:import/recommended",
     "plugin:import/typescript",
     "prettier",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:jsx-a11y/recommended",
   ],
   parserOptions: {
     project: ["tsconfig.json"],
@@ -17,12 +21,6 @@ module.exports = {
   settings: {
     react: {
       version: "detect",
-    },
-    "import/resolver": {
-      node: {
-        paths: ["src"],
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-      },
     },
   },
   ignorePatterns: ["build/", "dist/", "node_modules/", "*.js", "*.jsx"],
