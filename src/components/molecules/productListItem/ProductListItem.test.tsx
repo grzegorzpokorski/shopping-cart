fcaf74a1402f5c45b4254f9b0ff8b0cc7a41423b
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-02-07 19:45:04 +0100

    feat: add additional test to "ProductListItem"

--- src/components/molecules/productListItem/ProductListItem.test.tsx
+++ src/components/molecules/productListItem/ProductListItem.test.tsx
@@ -1,26 +1,63 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { ProductListItem } from "./ProductListItem";
 import { AppProviders } from "../../../providers/AppProviders";
-import data from "./../../../data/data.json";
+import { ProductType } from "../../../providers/ShoppingCartProvider";
 
-const productData = data[0];
+const product = {
+  id: 0,
+  name: "Logitech M330 silent",
+  price: 149.99,
+  category: "mysz",
+  image: {
+    url: "/images/logitech-m330-silent.jpg",
+    height: 532,
+    width: 600,
+    alt: "czarna myszka logitech m330 silent ukazana z góry",
+  },
+  availableAmount: 1,
+};
+const unavailableProduct = { ...product, availableAmount: 0 };
 
 describe("<ProductListItem>", () => {
-  beforeEach(() => {
+  const renderItem = (productData: ProductType) => {
     render(
       <AppProviders>
         <ProductListItem {...productData} />
       </AppProviders>,
     );
+  };
+
+  it("there is unable to add product to cart when available amount of it is equal to 0", () => {
+    renderItem(unavailableProduct);
+
+    const button = screen.getByText("Brak towaru");
+    expect(button).toBeVisible();
   });
 
   it("title is visible", () => {
-    const item = screen.getByText(productData.name);
+    renderItem(product);
+
+    const item = screen.getByText(product.name);
     expect(item).toBeVisible();
   });
 
   it("category is visible", () => {
-    const category = screen.getByText(productData.category);
+    renderItem(product);
+
+    const category = screen.getByText(product.category);
     expect(category).toBeVisible();
+  });
+
+  it("there is able to add product to cart", async () => {
+    renderItem(product);
+
+    const user = userEvent.setup();
+    const addToCartButton = screen.getByText("Dodaj do koszyka");
+
+    if (addToCartButton) {
+      await user.click(addToCartButton);
+      expect(addToCartButton.textContent).toBe("Zobacz w koszyku");
+    }
   });
 });
