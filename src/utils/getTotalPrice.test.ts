43cceb68d85cdd55c8c733c9f670712e2b203b12
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-02-07 23:10:38 +0100

    tests: add tests to "getTotalPrice" function

---
+++ src/utils/getTotalPrice.test.ts
@@ -0,0 +1,28 @@
+import { getTotalPrice } from "./getTotalPrice";
+
+const items = [
+  {
+    qty: 2,
+    price: 1.25,
+  },
+  {
+    qty: 1,
+    price: 156,
+  },
+  {
+    qty: 20,
+    price: 10,
+  },
+  {
+    qty: 56,
+    price: 10.2,
+  },
+];
+
+console.log(getTotalPrice(items));
+
+describe("getTotalPrice()", () => {
+  it("should return total price by given items", () => {
+    expect(getTotalPrice(items)).toEqual(Number(929.7).toFixed(2));
+  });
+});
