794e4ff7daae43719bbd577b57cde1c940550fb3
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-02-08 21:22:27 +0100

    fix: fix base url in development env

--- src/utils/getSiteUrl.ts
+++ src/utils/getSiteUrl.ts
@@ -1,5 +1,5 @@
 const productionHost = "shopping-cart-gp.vercel.app";
-const devHost = "localhost:5173";
+const devHost = "localhost:3000";
 const host = import.meta.env.MODE === "production" ? productionHost : devHost;
 
 const protocol = import.meta.env.MODE === "production" ? "https" : "http";
