eb581e2b1aa21654524ee1396c1bd8ea91579f7f
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-02-10 17:14:41 +0100

    fix: fix sorting products function

--- src/utils/getSortedProducts.ts
+++ src/utils/getSortedProducts.ts
@@ -3,7 +3,7 @@ import { ProductType, SortByType } from "../providers/ShoppingCartProvider";
 export const getSortedProducts = (products: ProductType[], by: SortByType) => {
   switch (by) {
     case "PRICE_ASC":
-      return products.sort((a, b) => (a.price > b.price ? 1 : -1));
+      return products.sort((a, b) => (a.price >= b.price ? 1 : -1));
     case "PRICE_DESC":
       return products.sort((a, b) => (a.price <= b.price ? 1 : -1));
     default:
