35624cd371d96ed0c36bfa8290f855a2a485966f
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-02-12 13:07:31 +0100

    fix: improve "useLocalStorage" types

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,18 +1,16 @@
 import { useEffect, useState } from "react";
-import { SafeParseReturnType, ZodType } from "zod";
+import { ZodType } from "zod";
 
 export const useLocalStorage = <T>(
   key: string,
   defaultValue: T,
-  shema: ZodType,
+  shema: ZodType<T>,
 ) => {
   const [value, setValue] = useState<T>(() => {
     const jsonValue = localStorage.getItem(key);
 
     if (jsonValue) {
-      const validate: SafeParseReturnType<unknown, T> = shema.safeParse(
-        JSON.parse(jsonValue),
-      );
+      const validate = shema.safeParse(JSON.parse(jsonValue));
       if (validate.success) return validate.data;
     }
     return defaultValue;
